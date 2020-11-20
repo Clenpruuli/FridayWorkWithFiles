130878b1ba936c58eb1210ba1021b3330278a40d
Opilane TTHK <opilane@tthk.ee>
2020-11-20 09:50:54 +0200

    first commit

---
+++ DeleteFile/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace DeleteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeleteFromFruits();
+            DeleteFromVegetables();
+        }
+
+        public static void DeleteFromFruits()
+        {
+            string rootPath = @"C:\Users\opilane\samples\LOGITpe20-Week12-WorkingWithFiles-Task\fruit";
+            string fileName = "toiletPaper.txt";
+
+            if (File.Exists(Path.Combine(rootPath, fileName)))
+            {
+                File.Delete(Path.Combine(rootPath, fileName));
+                Console.WriteLine("File deleted.");
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+
+        }
+
+        public static void DeleteFromVegetables()
+        {
+            string rootPath = @"C:\Users\opilane\samples\LOGITpe20-Week12-WorkingWithFiles-Task\vegetables";
+            string fileName = "cheese.txt";
+
+            if (File.Exists(Path.Combine(rootPath, fileName)))
+            {
+                File.Delete(Path.Combine(rootPath, fileName));
+                Console.WriteLine("File deleted.");
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+        }
+    }
+}
